af75f8ae1fc4d15c7d225ecc27839ea6e0f70eeb
Walter Philips <97041967+spaceexpanse@users.noreply.github.com>
2022-06-23 22:59:18 +0200

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -372,7 +372,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/tpruvot/yiimp
+    git clone -b xaya2 https://github.com/spaceexpanse/yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     sudo make
